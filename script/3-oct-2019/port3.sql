74ac94e1abc6d1ea22e804012cd25ee62577bb1b
shayan davodi <davodi.shayan@gmail.com>
2019-10-03 13:21:44 +0330

    add script

---
+++ script/3-oct-2019/port3.sql
@@ -0,0 +1,10 @@
+use [Scraper]
+INSERT INTO [dbo].[TblSite]
+           ([FldPkSite]
+           ,[FldName])
+     VALUES(4,'www.pilship.com')
+
+	 INSERT INTO [dbo].[TblSite]
+           ([FldPkSite]
+           ,[FldName])
+     VALUES(5,'www.zim.com')
